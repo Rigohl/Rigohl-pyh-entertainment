c15dc4f28b4424600284a49fbe7d19092e559581
Rigohl <rigobertohula@gmail.com>
2025-06-29 23:43:25 -0600

    11
    1

--- src/ai/dev.ts
+++
@@ -1,8 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-import '@/ai/flows/generate-song-lyrics-and-audio.ts';
-import '@/ai/flows/incorporate-user-requests-into-song.ts';
-import '@/ai/flows/generate-album-art.ts';
-
-    
